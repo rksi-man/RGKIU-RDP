f2f2c8f4a3782d0edc2d7e82d4cd9bcc0b8c04ff
VladKalambet <vlad.kalambet@yandex.ru>
2016-11-14 13:31:36 +0300

    exe

--- RGKIU/Form1.cs
+++ RGKIU/Form1.cs
@@ -483,7 +483,7 @@ namespace RDP
                     if (rdp.Connected.ToString() == "1")
                     rdp.Disconnect();
                     this.Width = 768;
-                    this.Height = 70;
+                    this.Height = 65;
                     address.Text = null;
                     login.Text = null;
                     /////
